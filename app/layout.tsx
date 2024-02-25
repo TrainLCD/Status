ca80f89e260a00cf01d539ba821ded2db0871c7d
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2024-02-26 08:35:49 +0900

    metadataBase

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://status.trainlcd.app"),
   title: "TrainLCD System Status",
   description: "TrainLCDサービスの障害情報",
 };
